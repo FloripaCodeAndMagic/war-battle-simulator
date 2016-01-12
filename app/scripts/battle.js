ff24fffeacf5a0c0cab21f3bf981d830050b1a23
Lucas Vieira <vieiralucas4@gmail.com>
2016-01-11 22:00:41 -0200

    Create Battle object constructor

---
+++ app/scripts/battle.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function Battle(attackRoll, defenseRoll) {
+    if (!Array.isArray(attackRoll) || !Array.isArray(defenseRoll)) {
+        throw new Error('Battle expected 2 arrays as arguments for contructor');
+    }
+
+    const sortFunc = function(a, b) {
+        return a < b;
+    };
+
+    this.attackRoll = attackRoll.sort(sortFunc);
+    this.defenseRoll = defenseRoll.sort(sortFunc);
+    this.attackLoss = 0;
+    this.defenseLoss = 0;
+}
+
+Battle.prototype.battle = function() {
+    while (this.attackRoll.length > this.defenseRoll.length) {
+        this.attackRoll.pop();
+    }
+
+    while (this.defenseRoll.length > this.attackRoll.length) {
+        this.defenseRoll.pop();
+    }
+
+    for (let i = 0; i < this.attackRoll.length; i++) {
+        if (this.attackRoll[i] > this.defenseRoll[i]) {
+            this.defenseLoss++;
+        } else {
+            this.attackLoss++;
+        }
+    }
+};
+
+module.exports = Battle;
---
+++ test/unit/battle.test.js
@@ -0,0 +1,146 @@
+'use strict'; 
+
+const Battle = require('../../app/scripts/battle');
+const chai = require('chai');
+const expect = chai.expect;
+
+chai.should();
+
+describe('Battle', () => {
+    describe('new', () => {
+        it('should only accept 2 arrays', () => {
+            const assertFn = function() {
+                return new Battle('lala', {hue: 'hue'});
+            };
+
+            expect(assertFn).to.throw(Error);
+        });
+
+        it('should set properties from parameters', () => {
+            const battle = new Battle([1, 2, 3], [3, 2, 1]);
+
+            battle.attackRoll.should.be.eql([3, 2, 1]);
+            battle.defenseRoll.should.be.eql([3, 2, 1]);
+            battle.attackLoss.should.be.eql(0);
+            battle.defenseLoss.should.be.eql(0);
+        });
+    });
+
+    describe('.battle', () => {
+        it('should set attackLoss and defenseLoss correctly', () => {
+            let battle = new Battle([3, 2, 1], [3, 2, 1]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(3);
+            battle.defenseLoss.should.be.eql(0);
+
+            battle = new Battle([4, 2, 1], [3, 2, 1]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(2);
+            battle.defenseLoss.should.be.eql(1);
+
+            battle = new Battle([4, 3, 1], [3, 2, 1]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(1);
+            battle.defenseLoss.should.be.eql(2);
+
+            battle = new Battle([4, 3, 2], [3, 2, 1]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(0);
+            battle.defenseLoss.should.be.eql(3);
+
+            battle = new Battle([4, 3, 2], [4, 3]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(2);
+            battle.defenseLoss.should.be.eql(0);
+
+            battle = new Battle([4, 3, 2], [3, 3]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(1);
+            battle.defenseLoss.should.be.eql(1);
+
+            battle = new Battle([4, 3, 2], [3, 2]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(0);
+            battle.defenseLoss.should.be.eql(2);
+
+            battle = new Battle([4, 3, 2], [4]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(1);
+            battle.defenseLoss.should.be.eql(0);
+
+            battle = new Battle([4, 3, 2], [3]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(0);
+            battle.defenseLoss.should.be.eql(1);
+
+            battle = new Battle([4, 3], [4, 3, 1]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(2);
+            battle.defenseLoss.should.be.eql(0);
+
+            battle = new Battle([4, 4], [4, 3, 1]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(1);
+            battle.defenseLoss.should.be.eql(1);
+
+            battle = new Battle([5, 4], [4, 3, 1]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(0);
+            battle.defenseLoss.should.be.eql(2);
+
+            battle = new Battle([5], [5, 3, 1]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(1);
+            battle.defenseLoss.should.be.eql(0);
+
+            battle = new Battle([5], [4, 3, 1]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(0);
+            battle.defenseLoss.should.be.eql(1);
+
+            battle = new Battle([5, 4], [5, 4]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(2);
+            battle.defenseLoss.should.be.eql(0);
+
+            battle = new Battle([5, 4], [4, 4]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(1);
+            battle.defenseLoss.should.be.eql(1);
+
+            battle = new Battle([5, 4], [4, 3]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(0);
+            battle.defenseLoss.should.be.eql(2);
+
+            battle = new Battle([5], [4]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(0);
+            battle.defenseLoss.should.be.eql(1);
+
+            battle = new Battle([5], [5]);
+
+            battle.battle();
+            battle.attackLoss.should.be.eql(1);
+            battle.defenseLoss.should.be.eql(0);
+        });
+    });
+});
