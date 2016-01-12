76f17a830d0c8aa55db08ab1651dd65adcf485e1
Lucas Vieira <vieiralucas4@gmail.com>
2016-01-11 22:59:59 -0200

    Finish BattleSimulator

--- app/scripts/battle-simulator.js
+++ app/scripts/battle-simulator.js
@@ -1,7 +1,35 @@
+'use strict';
+
+const dice = require('./utils/dice');
+
 function BattleSimulator(attackers, defenders, stopAt) {
     this.attackers = attackers;
     this.defenders = defenders;
-    this.stopAt = stopAt;
+    this.stopAt = stopAt || 1;
+    this.battles = [];
 }
+
+BattleSimulator.prototype.simulate = function() {
+    let battle;
+    let attackSize;
+    let defenseSize;
+
+    while (this.attackers > this.stopAt && this.defenders > 0) {
+        attackSize = this.attackers > 3 ? 3 : this.attackers - 1;
+        defenseSize = this.defenders > 3 ? 3 : this.defenders;
+        battle = this._createBattle(dice.roll(attackSize), dice.roll(defenseSize));
+        battle.battle();
+        this.battles.push(battle);
+        this.attackers -= battle.attackLoss;
+        this.defenders -= battle.defenseLoss;
+        this._log(battle);
+    }
+}
+
+BattleSimulator.prototype._createBattle = function(attackRoll, defenseRoll) {
+    return new Battle(attackRoll, defenseRoll);
+}
+
+BattleSimulator.prototype._log = console.log;
 
 module.exports = BattleSimulator;
---
+++ test/unit/battle-simulator.test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const BattleSimulator = require('../../app/scripts/battle-simulator');
+const Battle = require('../../app/scripts/battle');
+const chai = require('chai');
+const sinon = require('sinon');
+const sandbox = sinon.sandbox.create();
+
+chai.should();
+
+describe('BattleSimulator', () => {
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    describe('new', () => {
+        it('should set properties from parameters', () => {
+            const battleSimulator = new BattleSimulator(174, 67, 1);
+
+            battleSimulator.attackers.should.be.eql(174);
+            battleSimulator.defenders.should.be.eql(67);
+            battleSimulator.stopAt.should.be.eql(1);
+        });
+
+        it('should initialize battles as an empty array', () => {
+            const battleSimulator = new BattleSimulator(174, 67, 0);
+
+            battleSimulator.battles.should.be.a('array').with.length(0);
+        });
+    });
+
+    describe('.simulate', () => {
+        let battleSimulator;
+
+        beforeEach(() => {
+            battleSimulator = new BattleSimulator(10, 3, 1);
+            sandbox.stub(battleSimulator, '_log').returns();
+        });
+
+        it('should stop when attackers reach stopAt', () => {
+            const battle = new Battle([1, 1, 1], [2, 2, 2]);
+
+            sandbox.stub(battleSimulator, '_createBattle').returns(battle);
+            battleSimulator.simulate();
+            battleSimulator.attackers.should.be.eql(1);
+        });
+
+        it('should stop when defenders reach 0', () => {
+            const battle = new Battle([2, 2, 2], [1, 1, 1]);
+
+            sandbox.stub(battleSimulator, '_createBattle').returns(battle);
+            battleSimulator.simulate();
+            battleSimulator.defenders.should.be.at.most(0);
+        });
+
+        it('should call _log every battle', () => {
+            const battle = new Battle([2, 2, 2], [1, 1, 1]);
+
+            sandbox.stub(battleSimulator, '_createBattle').returns(battle);
+            battleSimulator.defenders = 4;
+            battleSimulator.simulate();
+            sinon.assert.calledTwice(battleSimulator._log)
+        });
+    });
+});
